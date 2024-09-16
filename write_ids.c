c86b4377d2266b46562ddf376f643a414d238892
ottoslade <52546313+ottoslade@users.noreply.github.com>
2024-09-16 15:03:42 -0400

    Update write_ids.c

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "ottoslade";
+    char STUDENT_ID[] = "999934733";
 
     return 0 ;
 }
